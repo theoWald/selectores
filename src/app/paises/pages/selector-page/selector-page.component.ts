698cf5c66d3c8170fb318cef44b5795951338094
Theo wald <theo83wald@gmail.com>
2021-06-02 17:59:52 +0200

    fin selectores

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -11,12 +11,17 @@ import { switchMap, tap } from 'rxjs/operators';
 export class SelectorPageComponent implements OnInit {
 
   miFormulario: FormGroup = this.fb.group({
-    region: ['', Validators.required],
-    pais: ['', Validators.required]
+    region    : ['', Validators.required],
+    pais      : ['', Validators.required],
+    frontera  : ['', Validators.required]
   })
 
-  regiones: string[] = [];
-  paises: PaisSmall[] = [];
+  regiones  : string[]    = [];
+  paises    : PaisSmall[] = [];
+  // fronteras : string[]    = [];
+  fronteras : PaisSmall[] = [];
+
+  cargando: boolean = false;
 
   constructor(private fb: FormBuilder,
               private paisesService: PaisesService) { }
@@ -37,10 +42,34 @@ export class SelectorPageComponent implements OnInit {
     //v.2
     this.miFormulario.get('region')?.valueChanges
       .pipe(
-        tap(( _ ) => { this.miFormulario.get('pais')?.reset('') }),
+        tap(( _ ) => { 
+          this.miFormulario.get('pais')?.reset('');
+          //this.miFormulario.get('frontera')?.disable();
+          this.cargando = true;
+        }),
         switchMap( region => this.paisesService.getPaisesPorRegion (region) )
       )
-      .subscribe( paises => { this.paises = paises; });
+      .subscribe( paises => { 
+        this.paises = paises; 
+        this.cargando = false;
+      });
+
+    this.miFormulario.get('pais')?.valueChanges
+        .pipe(
+          tap(() => { 
+            this.fronteras = [];
+            this.miFormulario.get('frontera')?.reset('');
+            //this.miFormulario.get('frontera')?.enable();
+            this.cargando = true;
+          }),
+          switchMap( codigo => this.paisesService.getPaisPorCodigo(codigo)),
+          switchMap( pais => this.paisesService.getPaisesPorCodigos(pais?.borders!))
+        )
+        .subscribe( paises => {
+          //this.fronteras = pais?.borders || [];    
+          this.fronteras = paises;    
+          this.cargando = false;         
+        });
   }
 
   guardar(){
--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { PaisSmall } from '../interfaces/paises.interface';
+import { combineLatest, Observable, of } from 'rxjs';
+import { Pais, PaisSmall } from '../interfaces/paises.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -20,7 +20,32 @@ export class PaisesService {
 
   getPaisesPorRegion( region: string ): Observable<PaisSmall[]>{
     return this.http.get<PaisSmall[]>(`${this.baseUrl}/region/${region}?fields=alpha3Code;name`);
-  }                                                                                                                           
+  }  
 
+  getPaisPorCodigo( codigo: string ): Observable<Pais | null>{   
+    if(!codigo){
+      return of(null);
+    }
+    return this.http.get<Pais>(`${this.baseUrl}/alpha/${codigo}`);
+  }
+  
+  getPaisPorCodigoSmall( codigo: string ): Observable<PaisSmall>{
+    return this.http.get<PaisSmall>(`${this.baseUrl}/alpha/${codigo}?fields=alpha3Code;name`);
+  }
 
+  getPaisesPorCodigos( borders: string[]): Observable<PaisSmall[]>{
+    if(!borders){
+      return of([]);
+    }
+
+    const peticiones: Observable<PaisSmall>[] = [];
+
+    borders.forEach(codigo => {
+      const peticion = this.getPaisPorCodigoSmall(codigo);
+      peticiones.push(peticion);
+    });
+
+    return combineLatest( peticiones );
+  }
+  
 }
